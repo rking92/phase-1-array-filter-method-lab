c59763dc1f874ce6d32f23623b61bc71bcd57a8f
Robert <rking92@gmail.com>
2021-05-15 03:51:58 -0400

    commit tree 1c2c30234f43e2fd8edc36585beefe1e6e6aab11

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (collection, drivers) {
+    for (const driver of collection) {
+    if (driversName(drivers)) {
+        console.log(drivers);
+    }
+        }
+    }
+
+    function driversName (drivers) {
+        return drivers
+    }
+
+
+    function fuzzyMatch (collection, drivers) {
+        return drivers.toLowerCase().indexOf()
+    }
